8c7692ec1a74e400a54703bd20a92a3e84f6baf0
Anees shaikh <aneesshaikh329@gmail.com>
2026-01-27 12:41:30 +0530

    bug fixes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,12 +1,12 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
-import User from "../models/User.model.js";
+import {User} from "../models/user.modles.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
   
         const {username, email, fullName, password} = req.body;
-        console.log('email: ', email);
+        
 
         if(
             [username, email, fullName, password].some((field) => field?.trim() === "")
@@ -19,17 +19,15 @@ const registerUser = asyncHandler(async (req, res) => {
         if(existedUser){
             throw new ApiError(409,"User with given email or username already exists");
         }
-        const avatarLocalPath = req.file?.avatar[0]?.path;
-        const coverImageLocalPath = req.file?.coverImage[0]?.path;
+        const avatarLocalPath = req.files?.avatar?.[0]?.path;
+        const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
 
         if(!avatarLocalPath ){
             throw new ApiError(400,"Avatar is required");
         }
 
         const avatar = await uploadOnCloudinary(avatarLocalPath);
-        const coverImage = coverImageLocalPath
-            ? await uploadOnCloudinary(coverImageLocalPath)
-            : null;
+        const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
         if (!avatar) {
             throw new ApiError(400, "avatar image is required");
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,14 @@
 import {Router} from "express";
 import {registerUser} from "../controllers/user.controllers.js";
-import { upload } from "../middlewares/multer.middlware.js";
+import { upload } from "../middlewares/multer.middleware.js"; 
+
 const router = Router();
 
 router.route('/register').post(
     upload.fields([
         {
-            name:"avtar",
-            maxCount:1
+            name: "avatar", 
+            maxCount: 1
         },
         {
             name: "coverImage",
