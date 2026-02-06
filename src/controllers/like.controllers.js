64b5a1db4036286ea611b23f39e682e791261cc0
Anees shaikh <aneesshaikh329@gmail.com>
2026-02-06 09:47:43 +0530

    Implement like functionality for videos and tweets with validation and response handling; add new route files for comments, dashboard, healthcheck, likes, playlists, subscriptions, tweets, and videos

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -1,13 +1,60 @@
-import mongoose from "mongoose"
+import mongoose,{isValidObjectId} from "mongoose"
 import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {Tweet} from "../models/tweet.models.js"
+import { Video } from "../models/video.models.js" 
+import { Comment } from "../models/comment.models.js" 
 
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+    if(!isValidObjectId(videoId))
+    {
+        throw new ApiError(400, "Invalid Video ID")
+    }
+    const video = await Video.findById(videoId)
+    if(!video)
+    {
+        throw new ApiError(404, "Video not found")
+    }
+
+    const existingLike = await video.findOne({
+        video: videoId,
+        likedBy: req.user._id
+    });
+
+    let isLiked;
+    let actionMessage;
+
+    if(existingLike)
+    {
+        await Like.deleteOne({_id: existingLike._id})
+        isLiked = false,
+        actionMessage = "Video unliked successfully"
+    }
+    else{
+        await Like.create({
+            video: videoId,
+            likedBy: req.user._id
+        });
+        isLiked = true
+        actionMessage = "Video liked successfully"
+    }
+    const totalLikes = await Like.countDocument({video: videoId})
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,{
+            liked: isLiked,
+            totalLikes,
+            videoId
+            }, actionMessage)
+    )
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
@@ -19,6 +66,55 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
+    if(!isValidObjectId(tweetId))
+    {
+        throw new ApiError(400, "Invalid Tweet ID")
+    }
+
+    const tweet = await Tweet.findByID(tweetId)
+
+    if(!tweet)
+    {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    const existingLike = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user._id
+    });
+
+    let isLiked;
+    let actionMessage;
+
+    if(existingLike)
+    {
+        await Like.deleteOne({_id: existingLike._id});
+        isLiked = false;
+        actionMessage ="Tweet unliked successfully"
+    }
+    else{
+        await Like.create({
+            tweet: tweetId,
+            likedBy: req.user._id
+        });
+        isLiked= true;
+        actionMessage= "Tweet liked successfully"
+    }
+
+    const totalLikes =  await Like.countDocument({tweet: tweetId});
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,{
+            liked: isLiked,
+            totalLikes,
+            tweetId
+            }, actionMessage)
+    )
+
+
 }
 )
 
