352dfd63fe9c533b6fed11dbffc5df0f89bb1143
Anees shaikh <aneesshaikh329@gmail.com>
2026-02-25 09:42:50 +0530

    Import correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/SideBar";
+import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
